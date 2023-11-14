d3f6935fd48098dc78da05c70e61c5abd47f9411
Vishota <88852731+Vishota@users.noreply.github.com>
2023-11-14 16:22:25 +0300

    1.0.10 (asyncSafe)

---
+++ src/asyncSafe.ts
@@ -0,0 +1,23 @@
+import crypto from 'crypto';
+
+export function asyncSafe<R extends (...args: any[]) => any>(func: R): (...args: Parameters<typeof func>) => Promise<ReturnType<R>> {
+    let busy = false;
+    const safe: ReturnType<typeof asyncSafe> = async (...args) => {
+        // waiting for "busy" equal to false
+        const freePromise = new Promise((resolver) => {
+            const waiting = setInterval(() => {
+                if (!busy) {
+                    resolver(0)
+                    busy = true
+                    clearInterval(waiting)
+                }
+            }, 100)
+        })
+        await freePromise;
+        const res = await func()
+        busy = false;
+        return res
+    }
+
+    return safe
+}
